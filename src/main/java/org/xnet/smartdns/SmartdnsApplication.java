e2dc8d29ad9fa77f2519e3937db09c8804544093
xiexiaoqun <xiexiaoqun@yeah.net>
2022-11-30 06:12:17 +0800

    init spring project

---
+++ src/main/java/org/xnet/smartdns/SmartdnsApplication.java
@@ -0,0 +1,13 @@
+package org.xnet.smartdns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartdnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartdnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/xnet/smartdns/SmartdnsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xnet.smartdns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartdnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
